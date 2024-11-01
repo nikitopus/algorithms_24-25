08f2cb505026483447b23204e458061bd4a53133
Никита <nikitopus05@mail.ru>
2024-11-01 07:27:30 +0300

    3 laba is ready, but without review.

---
+++ laba_3/nik/find_salt.py
@@ -0,0 +1,33 @@
+import time
+
+def find_salt():
+    with open("output.txt", "r", encoding="utf-8") as f:
+        d = dict()
+        phones = [89156311602, 89678395615, 89859771985, 89109807351, 89108471943]
+
+        for el in f:
+            el = el.split(":")[-1]
+            for i in range(5):
+                if (int(el) - phones[i]) not in d.keys():
+                    d[(int(el) - phones[i])] = 1
+                else:
+                    d[(int(el) - phones[i])] += 1
+        for key, value in d.items():
+            if value >= 2:
+                salt = key
+                return salt
+                
+def write_right_phones(salt):
+    with open("right_phone_number.txt", "w", encoding="utf-8") as f:
+        lst = []
+        with open("output.txt", "r", encoding="utf-8") as ff:
+            for el in ff:
+                el = int(el.split(":")[-1])
+                lst.append(str(el - salt))
+        s = "\n".join(lst)
+        f.write(s)
+
+if __name__ == "__main__":
+    salt = find_salt()
+    write_right_phones(salt)
+    print (salt)
